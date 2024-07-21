9c435292088ab3f86a982f0af85a258c81470b11
Robert Mason <robertmasonphilpapers@gmail.com>
2024-07-21 08:41:44 -0400

    shortcut self comparisons

--- lib/Biblio/Citation/Compare.pm
+++ lib/Biblio/Citation/Compare.pm
@@ -107,8 +107,9 @@ my %book_format = (
 );
 
 sub sameWork {
-
  	my ($e, $c, $threshold,$loose,$nolinks,%opts) = @_;
+    return 1 if defined $e->id && $e->id ne "" and $e->{id} eq $c->{id};
+    
   	return 0 if (!$c || !$e);
 
     my $debug = $opts{debug} || 0;
